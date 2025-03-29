7652d94ac688145a78b675fce7b7494c00da9ce3
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-03-30 01:30:15 +0500

    Build Dynamic Blog Page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,22 +1,9 @@
-// import { getPost } from '@/sanity/lib/queries'
-// import { Post } from '@/components/main/Post'
+import React from 'react'
 
-// interface PostPageProps {
-//   params: {
-//     slug: string
-//   }
-// }
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
 
-// export default async function PostPage({ params }: PostPageProps) {
-//   const post = await getPost(params.slug)
-
-//   if (!post) {
-//     return (
-//       <div className="max-w-3xl mx-auto px-4 py-8 text-center">
-//         <h1 className="text-2xl font-bold text-gray-900">Post not found</h1>
-//       </div>
-//     )
-//   }
-
-//   return <Post post={post} />
-// }
+export default page
