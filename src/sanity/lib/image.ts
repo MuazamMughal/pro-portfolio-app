3bca6699ef383c59400341b6707e8eaf0e42ce9b
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-04-07 02:41:35 +0500

    update sanity image

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,14 +1,4 @@
-// import createImageUrlBuilder from '@sanity/image-url'
-// import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-// import { dataset, projectId } from '../env'
-
-// // https://www.sanity.io/docs/image-url
-// const builder = createImageUrlBuilder({ projectId, dataset })
-
-// export const urlFor = (source: SanityImageSource) => {
-//   return builder.image(source)
-// }
 
 import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
